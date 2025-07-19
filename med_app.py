a3650ea18e6f57df9b1530c36c21bdf5860eb3a2
rainahckhan <156608452+rainahckhan@users.noreply.github.com>
2025-07-19 19:58:44 -0400

    Update med_app.py

--- med_app.py
+++ med_app.py
@@ -191,7 +191,6 @@ The developers and providers of this app disclaim all liability for any damages 
 <br><br>
 """, unsafe_allow_html=True)
 
-st.markdown("<br>", unsafe_allow_html=True)
 
 st.write("Enter an illness to see medications with possible safety concerns related to it.")
 
