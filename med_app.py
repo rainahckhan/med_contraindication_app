7d537f63ba7f5e23860f21790cd8c04ab44e54b2
rainahckhan <156608452+rainahckhan@users.noreply.github.com>
2025-07-19 19:54:20 -0400

    Update med_app.py

--- med_app.py
+++ med_app.py
@@ -191,7 +191,7 @@ The developers and providers of this app disclaim all liability for any damages 
 <br><br>
 """, unsafe_allow_html=True)
 
-st.write("Enter an illness to see medications with possible safety concerns related to it.")
+st.write("Enter an illness to see medications with possible safety concerns related to it.<br><br>")
 
 user_input = st.text_input(
     "Enter disease or illness:",
